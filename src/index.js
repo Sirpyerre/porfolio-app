1a35708eef37a39b75192d72b88705a63df7c9dd
Pedro Rojas <pedro.rojas@gmail.com>
2019-06-11 18:21:43 -0500

    metas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, Switch, Route} from "react-router-dom";
-import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 // import Jokes from './components/Jokes';
 import './index.css';
