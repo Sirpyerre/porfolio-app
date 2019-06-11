0cf6b80d7931ad1bafb41b2b28d2fb1fa7718ab8
Pedro Rojas <pedro.rojas@gmail.com>
2019-06-11 18:16:21 -0500

    quitar react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,15 +3,7 @@ import ReactDom from 'react-dom';
 import { Router, Switch, Route} from "react-router-dom";
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
-import Jokes from './components/Jokes';
+// import Jokes from './components/Jokes';
 import './index.css';
 
-
-ReactDom.render(
-    <Router history={createBrowserHistory()}>
-        <Switch>
-            <Route exact={true} path='/' component={App} />
-            <Route path='/jokes' component={Jokes} />
-        </Switch>
-    </Router>
-    , document.getElementById('root'));
+ReactDom.render(<App></App>, document.getElementById('root'));
